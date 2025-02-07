505ef53d8f025e24a6fe5df8a26b0230bedc4587
ChangSonia <79300461+ChangSonia@users.noreply.github.com>
2025-02-07 17:44:58 +0000

    finish

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,5 +1,25 @@
 class DiceController < ApplicationController
+  def home
+    render({:template => "dice_templates/home"})
+  end
+
   def two_six
+    @nums = 2.times.map{Random.rand(1..6)} 
     render({:template => "dice_templates/2d6"})
+  end
+
+  def two_ten
+    @nums = 2.times.map{Random.rand(1..10)} 
+    render({:template => "dice_templates/2d10"})
+  end
+
+  def one_twenty
+    @nums = 1.times.map{Random.rand(1..20)} 
+    render({:template => "dice_templates/1d20"})
+  end
+
+  def five_four
+    @nums = 5.times.map{Random.rand(1..4)} 
+    render({:template => "dice_templates/5d4"})
   end
 end
